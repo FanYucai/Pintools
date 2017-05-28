40e39b7e8d4285fa7d919f3b5dd003e894f276cc
FanYucai <fanyucai@126.com>
2017-05-28 07:48:59 -0700

    almost task 1 over

--- pin-3.2-81205-gcc-linux/source/tools/ManualExamples/a.cpp
+++ pin-3.2-81205-gcc-linux/source/tools/ManualExamples/a.cpp
@@ -4,39 +4,119 @@
 #include <string.h>
 #include <set>
 #include <map>
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
 #include "pin.H"
+
+extern "C" {
+	#include "xed-interface.h"
+}
 
 
 ofstream OutFile;
 
 // The running count of instructions is kept here
 // make it static to help the compiler optimize docount
-static UINT64 inscount[10000000] = {};
-static string insname[10000000] = {};
+
 static UINT64 instot = 0;
-static UINT64 setIter = 0;
-static string setStr = "";
+static string insStr = "";
+static string addrClass = "";
+static UINT64 imdtBit = 0;
 
-//static UINT64 findcount[1000] = {};
-//static string findclass[1000] = {};
-//static UINT64 findtot = 0;
+std::map<string, UINT64> InsMap;
+std::map<string, UINT64> AddrMap;
+std::map<UINT64, UINT64> ImdtMap;
 
-//static UINT64 datacount[1000] = {};
-//static string dataclass[1000] = {};
-//static UINT64 datatot = 0;
+
+static void OnInstruction(ADDRINT unsigned_value, long signed_value, bool is_signed, INT32 length_bits)
+{
+    OutFile << "Contains an immediate value: " << "0x" << hex << setfill('0') << setw(length_bits/4)
+             << unsigned_value << setfill(' ') << setw(1) << dec;
+    if (is_signed)
+    {
+        OutFile << " (signed: " << signed_value << "),";
+    }
+    OutFile << " operand is " << length_bits << " bits long." << endl;
+}
+
+
+/*
+ * A routine to query the immediate operand per instruction. May be called at
+ * instrumentation or analysis time.
+ */
+VOID GetOperLenAndSigned(INS ins, INT32 i, INT32& length_bits, bool& is_signed)
+{
+    xed_decoded_inst_t* xedd = INS_XedDec(ins);
+    // To print out the gory details uncomment this:
+    // char buf[2048];
+    // xed_decoded_inst_dump(xedd, buf, 2048);
+    // OutFile << buf << endl;
+    length_bits = 8*xed_decoded_inst_operand_length(xedd, i);
+    is_signed = xed_decoded_inst_get_immediate_is_signed(xedd);
+}
 
 // This function is called before every instruction is executed
-VOID do_inscount() 
-{ 
-    inscount[setIter]++;
-    insname[instot++] = setStr;
-}
-    
+
 // Pin calls this function every time a new instruction is encountered
 VOID Instruction(INS ins, VOID *v)
 {	
-    setStr = INS_Mnemonic(ins);
-    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)do_inscount, IARG_END);
+	// instructions
+    insStr = INS_Mnemonic(ins);
+    if(InsMap.count(insStr)>0) {
+    	InsMap[insStr]++;
+    } else {
+    	InsMap[insStr] = 1;
+    }
+    OutFile << insStr << " ";
+    
+
+    // addr
+    const xed_decoded_inst_t* xedd = INS_XedDec(ins);
+    const xed_iform_enum_t index=xed_decoded_inst_get_iform_enum(xedd);
+    const char* buff=xed_iform_enum_t2str(index);
+    addrClass = buff;
+    if(AddrMap.count(addrClass)>0) {
+    	AddrMap[addrClass]++;
+    } else {
+    	AddrMap[addrClass] = 1;
+    }
+
+
+    // imdt figure:
+
+    UINT32 n = INS_OperandCount(ins);
+    for(UINT32 i=0; i<n; i++) {
+    	if(INS_OperandIsImmediate(ins, i)) {
+    		// Get the value itself
+            ADDRINT value = INS_OperandImmediate(ins, i);
+            long signed_value = (long)value;
+            // Get length information
+            INT32 length_bits = -1;
+            bool is_signed = false;
+            GetOperLenAndSigned(ins, i, length_bits, is_signed);
+            OnInstruction(value, signed_value, is_signed, length_bits);
+    		OutFile << "imm" << ", ";
+    	}
+    	if(INS_OperandIsReg(ins, i)) 
+    		{OutFile << "reg" << ", ";}
+    	if(INS_OperandIsMemory(ins, i)) 
+    		{OutFile << "mem" << ", ";}
+    	if(INS_OperandIsImplicit(ins, i)) 
+    		{OutFile << "imp" << ", ";}	
+    }
+    OutFile << endl;
+
+    if(ImdtMap.count(imdtBit)>0) {
+    	ImdtMap[imdtBit]++;
+    } else {
+    	ImdtMap[imdtBit] = 1;
+    }
+
+
+    // INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)do_inscount, IARG_END);
     // INS_InsertCall(ins, IPOINT_AFTER, (AFUNPTR)do_findcount, IARG_END);
     // INS_InsertCall(ins, IPOINT_AFTER, (AFUNPTR)do_datacount, IARG_END);
 
@@ -50,14 +130,38 @@ VOID Fini(INT32 code, VOID *v)
 {
     // Write to a file since cout and cerr maybe closed by the application
     OutFile.setf(ios::showbase);
-    //OutFile << "insCount " << inscount << endl;
-    //OutFile << "findCount " << findcount << endl;
-    //OutFile << "dataCount " << datacount << endl;
     
-    
-    for(UINT64 i=0; i<instot; i++) 
-    {
-        OutFile << "ins #" << i << ": " << insname[i] << endl;
+	// //map operations
+ //    for(std::map<string, UINT64>::iterator it=InsMap.begin(); it != InsMap.end(); ++it) {
+ //    	instot += it->second;
+ //    }
+ //    OutFile << "tot: " << instot << endl;
+ //    for(std::map<string, UINT64>::iterator it=InsMap.begin(); it != InsMap.end(); ++it) {
+ //    	OutFile << "name: " << it->first << 
+ //    	", count: " << it->second <<
+ //    	", percent: " << it->second / (0.01*instot) << "%" << endl;
+ //    }
+
+ //   	//map operations
+ //    for(std::map<string, UINT64>::iterator it=AddrMap.begin(); it != AddrMap.end(); ++it) {
+ //    	instot += it->second;
+ //    }
+ //    OutFile << "tot: " << instot << endl;
+ //    for(std::map<string, UINT64>::iterator it=AddrMap.begin(); it != AddrMap.end(); ++it) {
+ //    	OutFile << "name: " << it->first << 
+ //    	", count: " << it->second <<
+ //    	", percent: " << it->second / (0.01*instot) << "%" << endl;
+ //    }
+
+   	//map operations
+    for(std::map<UINT64, UINT64>::iterator it=ImdtMap.begin(); it != ImdtMap.end(); ++it) {
+    	instot += it->second;
+    }
+    OutFile << "tot: " << instot << endl;
+    for(std::map<UINT64, UINT64>::iterator it= ImdtMap.begin(); it != ImdtMap.end(); ++it) {
+    	OutFile << "name: " << it->first << 
+    	", count: " << it->second <<
+    	", percent: " << it->second / (0.01*instot) << "%" << endl;
     }
     OutFile.close();
 }
